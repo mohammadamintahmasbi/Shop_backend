712fd260b8f7e14177cf1573735721807a521144
mohammad amin <mat.chenari.computer@gmail.com>
2024-01-23 13:17:41 +0330

    debug the edit user api

--- Shopify/shopify_apps/users/serializers.py
+++ Shopify/shopify_apps/users/serializers.py
@@ -5,7 +5,7 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["email", "password"]
+        fields = ["email", "password", "image", "first_name", "last_name"]
 
     # set_password use to save password hashed in db
     def create(self, validate_data):
@@ -13,3 +13,23 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(validate_data["password"])
         user.save()
         return user
+    
+    def update(self, instance, validated_data):
+        if "email" in validated_data:
+            instance.email = validated_data.get("email", instance.email)
+        
+        if "image" in validated_data:
+            instance.image = validated_data.get("image", instance.image)
+        
+        if "first_name" in validated_data:
+            instance.first_name = validated_data.get("first_name", instance.first_name)
+        
+        if "last_name" in validated_data:
+            instance.last_name = validated_data.get("last_name", instance.last_name)
+            
+        if "password" in validated_data:
+            instance.set_password(validated_data["password"])
+        
+        instance.save()
+        return instance
+        
--- Shopify/shopify_apps/users/views.py
+++ Shopify/shopify_apps/users/views.py
@@ -1,4 +1,5 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, DestroyAPIView, UpdateAPIView
+from rest_framework.permissions import IsAdminUser
 
 from .serializers import UserSerializer
 from .models import User
@@ -6,5 +7,27 @@ from .models import User
 class CreateUserView(CreateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = []
     
+class ListUserView(ListAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAdminUser]
     
+class GetUserview(RetrieveAPIView):
+    lookup_field = "id"
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAdminUser]
+
+class UpdateUserView(UpdateAPIView):
+    lookup_field = "id"
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAdminUser]
+
+class DeleteUserView(DestroyAPIView):
+    lookup_field = "id"
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAdminUser]    
